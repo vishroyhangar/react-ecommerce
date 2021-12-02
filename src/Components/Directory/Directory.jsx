ec258523bca706ecf0a098b29fc67f4e706d7731
vishroy <vseenara@proximitybbdo.fr>
2021-12-02 18:40:11 +0400

    Homepage container done

---
+++ src/Components/Directory/Directory.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './Directory.scss';
+import MenuItem from '../MenuItem/MenuItem.jsx';
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: 'Hats',
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          id: 1
+        },
+        {
+          title: 'Jackets',
+          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          id: 2
+        },
+        {
+          title: 'Sneakers',
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          id: 3
+        },
+        {
+          title: 'Womem',
+          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          id: 4
+        }, {
+          title: 'Men',
+          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          id: 5
+        }
+      ]
+    }
+  }
+
+  render() {
+    return(
+      <div className="directory-menu">
+        {
+          this.state.sections.map(({title, imageUrl, id}) => {
+            return <MenuItem key={id} title={title} imageUrl={imageUrl}/>
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default Directory;
---
+++ src/Pages/Homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Homepage.scss';
+import Directory from '../../Components/Directory/Directory.jsx';
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  )
+}
+
+export default Homepage;
